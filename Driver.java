d4e5a080d93dba5ce9767ae1fc6e92b92f8c720f
Yucheng Cheng <yuchengcheng@YuchengdeMacBook-Pro.local>
2023-09-26 00:15:56 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,47 @@
+import java.io.*;
+
 public class Driver {
-	public static void main(String [] args) {
+	public static void main(String [] args) throws IOException {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		double [] c1_c = {6,5};
+		int [] c1_e = {0,3};
+		Polynomial p1 = new Polynomial(c1_c,c1_e);
+		double [] c2_c = {-2,-9};
+		int [] c2_e = {1,4};
+		Polynomial p2 = new Polynomial(c2_c,c2_e);
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
 		if(s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
+		
+		double [] c3_c = {2,-4.8,4};
+		int [] c3_e = {1,2,5};
+		Polynomial p3 = new Polynomial(c3_c,c3_e);
+		double [] c4_c = {3,2,4};
+		int [] c4_e = {1,4,6};
+		Polynomial p4 = new Polynomial(c4_c,c4_e);
+		Polynomial prod = p1.multiply(p3);
+		System.out.println("prod:");
+		for(int i=0;i<prod.coeff.length;++i) {
+			System.out.println("coeff expo: "+prod.coeff[i]+" "+prod.expo[i]);
+		}
+		System.out.println();
+		System.out.println("prod:");
+		prod = p3.multiply(p4);
+		for(int i=0;i<prod.coeff.length;++i) {
+			System.out.println("coeff expo: "+prod.coeff[i]+" "+prod.expo[i]);
+		}
+		System.out.println();
+		
+		File f=new File("/Users/yuchengcheng/b07lab1/input.txt");
+		Polynomial p5=new Polynomial(f);
+		System.out.println("p5:");
+		for(int i=0;i<p5.coeff.length;++i) {
+			System.out.println("coeff expo: "+p5.coeff[i]+" "+p5.expo[i]);
+		}
+		System.out.println();
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,40 +1,153 @@
+import java.io.*;
+
 public class Polynomial {
-	double[] a;
+	double[] coeff;
+	int[] expo;
 	public Polynomial() {
-		a=new double[1];
+		coeff=new double[1];
+		expo=new int[1];
 	}
-	public Polynomial(double[] b) {
-		int b_le=b.length;
-		a=new double[b_le];
-		for(int i=0;i<b_le;i++){
-			a[i]=b[i];
+	public Polynomial(double[] a,int[] b) {
+		coeff=new double[a.length];
+		for(int i=0;i<a.length;i++){
+			coeff[i]=a[i];
+		}
+		expo=new int[b.length];
+		for(int i=0;i<b.length;i++){
+			expo[i]=b[i];
 		}
 	}
-	public Polynomial add(Polynomial b){
-		int b_le=b.a.length,a_le=a.length;
-		Polynomial mx,mn;
-		if(a_le>=b_le){
-			mx=new Polynomial(a);
-			mn=new Polynomial(b.a);
+	public boolean chk(double x) {
+		return Math.abs(x)<1e-8;
+	}
+	public Polynomial add(Polynomial a){
+		int max_expo=Math.max(expo[expo.length-1],a.expo[a.expo.length-1]);
+		Polynomial b=new Polynomial();
+		b.coeff=new double[max_expo+5];
+		b.expo=new int[max_expo+5];
+		int pos=0,pos_a=0,size_b=0;
+		for(int i=0;i<=max_expo;++i) {
+			double val=0;
+			if(pos<expo.length && expo[pos]==i) {
+				val+=coeff[pos];++pos;
+			}
+			if(pos_a<a.expo.length && a.expo[pos_a]==i) {
+				val+=a.coeff[pos_a];++pos_a;
+			}
+			if(!chk(val)) {
+				b.coeff[size_b]=val;
+				b.expo[size_b]=i;
+				size_b+=1;
+			}
 		}
-		else{
-			mx=new Polynomial(b.a);
-			mn=new Polynomial(a);
+		Polynomial c=new Polynomial();
+		c.coeff=new double[size_b];
+		c.expo=new int[size_b];
+		for(int i=0;i<size_b;++i) {
+			c.coeff[i]=b.coeff[i];
+			c.expo[i]=b.expo[i];
 		}
-		for(int i=0;i<mn.a.length;i++)
-			mx.a[i]+=mn.a[i];
-		return mx;
+		return c;
 	}
 	public double evaluate(double x){
-		int a_le=a.length;
 		double res=0;
-		for(int i=0;i<a_le;++i){
-			//System.out.println(a[i]);
-			res+=a[i]*Math.pow(x,i);
+		for(int i=0;i<coeff.length;++i){
+			res+=coeff[i]*Math.pow(x,expo[i]);
 		}
 		return res;
 	}
 	public boolean hasRoot(double x){
-		return evaluate(x)==0.0;
+		return chk(evaluate(x));
+	}
+	public Polynomial multiply(Polynomial a) {
+		int max_expo=expo[expo.length-1]+a.expo[a.expo.length-1];
+		Polynomial b=new Polynomial();
+		b.coeff=new double[max_expo+5];
+		b.expo=new int[max_expo+5];
+		for(int i=0;i<=max_expo;++i) {
+			b.coeff[i]=0;
+			b.expo[i]=i;
+		}
+		for(int i=0;i<coeff.length;++i) {
+			for(int j=0;j<a.coeff.length;++j) {
+				b.coeff[expo[i]+a.expo[j]]+=coeff[i]*a.coeff[j];
+			}
+		}
+		int size_c=0;
+		for(int i=0;i<=max_expo;++i)if(!chk(b.coeff[i]))size_c+=1;
+		Polynomial c=new Polynomial();
+		c.coeff=new double[size_c];
+		c.expo=new int[size_c];
+		size_c=0;
+		for(int i=0;i<=max_expo;++i) {
+			if(!chk(b.coeff[i])) {
+				c.coeff[size_c]=b.coeff[i];
+				c.expo[size_c]=b.expo[i];
+				size_c+=1;
+			}
+		}
+		return c;
+	}
+	public int find_op(String s) {
+		for(int i=0;i<s.length();++i) {
+			if(s.charAt(i)=='+' || s.charAt(i)=='-')return i;
+		}
+		return -1;
+	}
+	public Polynomial(File f) throws IOException{
+		BufferedReader b=new BufferedReader(new FileReader(f));
+		String Line=b.readLine();
+		String[] s=Line.split("x");
+		Polynomial a=new Polynomial();
+		a.coeff=new double[s.length+5];
+		a.expo=new int[s.length+5];
+		int size=0,op_pos=find_op(s[0]);
+		if(op_pos==-1 || op_pos==0) {
+			a.coeff[size]=Double.parseDouble(s[0]);
+		}else {
+			a.coeff[size]=Double.parseDouble(s[0].substring(0,op_pos));
+			a.expo[size]=0;
+			++size;
+			a.coeff[size]=Double.parseDouble(s[0].substring(op_pos));
+		}
+		for(int i=1;i<s.length;++i) {
+			op_pos=find_op(s[i]);
+			if(op_pos==-1) {
+				a.expo[size]=Integer.parseInt(s[i]);
+				++size;
+			}else if(op_pos==0){
+				a.expo[size]=1;
+				++size;
+				a.coeff[size]=Double.parseDouble(s[i]);
+			}else {
+				a.expo[size]=Integer.parseInt(s[i].substring(0,op_pos));
+				++size;
+				a.coeff[size]=Double.parseDouble(s[i].substring(op_pos));
+			}
+		}
+		if(Line.charAt(Line.length()-1)=='x') {
+			a.expo[size]=1;
+			++size;
+		}
+		coeff=new double[size];
+		expo=new int[size];
+		for(int i=0;i<size;++i) {
+			coeff[i]=a.coeff[i];
+			expo[i]=a.expo[i];
+		}
+	}
+	public void saveToFile(String path) throws IOException {
+		String s="";
+		for(int i=0;i<coeff.length;++i) {
+			if(expo[i]==0) {
+				s+=coeff[i];
+				continue;
+			}
+			s+=coeff[i];
+			s+="x";
+			s+=expo[i];
+		}
+		FileWriter f=new FileWriter(path);
+		f.write(s);
 	}
 }
